43f4f11a319ffb51093b9500b6895c4844ec47ff
dinesh lama <dinesh99981@gmail.com>
2020-05-14 07:23:31 +0545

    latest joy publisher based on ros geomtry msg twist

--- joy_publisher/joy_publisher.ino
+++ joy_publisher/joy_publisher.ino
@@ -24,13 +24,33 @@ void loop()
   int xAxis = analogRead(A0);
   int yAxis = analogRead(A1);
    
-  joy_data.linear.x = xAxis;
+  if (yAxis < 470) {
+    yAxis = -map(yAxis, 470, 0, 0, 255);
+  }
+  else if (yAxis > 550) {
+    yAxis = map(yAxis, 550, 1023, 0, 255);
+  }
+  else {
+    yAxis = 0;
+  }
+  
+  if (xAxis < 470) {
+    xAxis = -map(xAxis, 470, 0, 0, 255);
+  }
+  else if (xAxis > 550) {
+    xAxis = map(xAxis, 550, 1023, 0, 255);
+  }
+  else {
+    xAxis = 0;
+  }
+  
+  joy_data.linear.x = 0.0;
   joy_data.linear.y = yAxis;
-  joy_data.linear.z = 11;  
+  joy_data.linear.z = 0.0;  
 
-  joy_data.angular.x = xAxis;
-  joy_data.angular.y = yAxis;
-  joy_data.angular.z = 11;
+  joy_data.angular.x = 0.0;
+  joy_data.angular.y = 0.0;
+  joy_data.angular.z = xAxis;
   
   p.publish(&joy_data);
   nh.spinOnce();
