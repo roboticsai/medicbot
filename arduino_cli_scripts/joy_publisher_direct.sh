bcfab571cbbde1f71e618c07b2ef15cb492bfc35
dinesh999lama <dinesh99981@gmail.com>
2020-05-18 09:25:38 +0000

    some changes

--- arduino_cli_scripts/joy_publisher_direct.sh
+++ arduino_cli_scripts/joy_publisher_direct.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-arduino-cli compile --fqbn arduino:avr:uno joy_publisher
-arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:uno joy_publisher
+arduino-cli compile --fqbn arduino:avr:uno joy_publisher_direct
+arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:uno joy_publisher_direct
 
--- arduino_cli_scripts/joy_subscriber_direct.sh
+++ arduino_cli_scripts/joy_subscriber_direct.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328oldi joy_subscriber
-arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328old joy_subscriber
+arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328oldi joy_subscriber_direct
+arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328old joy_subscriber_direct
