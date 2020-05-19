c722556cce23c017f47a3f56709fd8539a160946
dinesh999lama <dinesh99981@gmail.com>
2020-05-19 13:23:38 +0100

    changes

--- master.sh
+++ master.sh
@@ -3,4 +3,4 @@ source /opt/ros/melodic/setup.bash
 source /home/odroid/medicbot/devel/setup.bash
 export ROS_IP=192.168.1.102
 export ROS_MASTER_URI=http://192.168.1.102:11311
-roslaunch catkin_pkg catkin.launch
+roslaunch catkin_pkg master.launch
